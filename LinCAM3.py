3d6124725a3fa8439eebb0d77b1b870b1e2707d3
dandfmd <daniel@dfmd.mx>
2019-12-20 14:42:26 -0600

    Optimized circular box

--- LinCAM3.py
+++ LinCAM3.py
@@ -35,7 +35,7 @@ from collections import OrderedDict
 import traceback
 
 COMMAND_NAME = "LinCAM3"
-VERSION = "Lin-3.3_dev_2019"
+VERSION = "Lin-3.3b_2019"
 WEBPAGE = 'https://github.com/dfmdmx/Rhino_LinCAM3'
     
 # SampleEtoRoomNumber dialog class
